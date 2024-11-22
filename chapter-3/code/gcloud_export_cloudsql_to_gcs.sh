87bcc11670f880e78290d699e34a15b4f3167d01
Thilakreddy515 <159428023+Thilakreddy515@users.noreply.github.com>
2024-11-22 11:09:48 +0530

    Update gcloud_export_cloudsql_to_gcs.sh

--- chapter-3/code/gcloud_export_cloudsql_to_gcs.sh
+++ chapter-3/code/gcloud_export_cloudsql_to_gcs.sh
@@ -1,4 +1,4 @@
-bucket_name=[your gcs bucket name]
+bucket_name=[packt-data-eng-on-gcp-data-bucket11]
 
 gcloud sql export csv mysql-instance-source \
 gs://$bucket_name/mysql_export/stations/20180101/stations.csv \
